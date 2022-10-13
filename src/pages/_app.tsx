3b12d89c5f9e01772629b802de235cb5b343c386
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2022-10-13 19:38:55 -0300

    config: [basic] chakra, query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "../services/queryClient";
+import { theme } from "../styles/theme";
 
 export default function ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }

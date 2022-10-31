78c9d0a5eb411121e15529c52b9b818cc977c513
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2022-10-31 14:29:29 -0300

    fix: input trailing visual bug

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -19,25 +19,27 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInputProps> = (
   ref
 ) => {
   return (
-    <div className="relative">
-      <label htmlFor={name} className="sr-only">
-        {label ?? name}
-      </label>
-      <input
-        id={name}
-        name={name}
-        placeholder={placeholder}
-        {...props}
-        ref={ref}
-        className={`relative block w-full ${
-          !!children && "pr-8"
-        } appearance-none rounded-md border bg-gray-400 bg-opacity-10 dark:border-gray-600 border-gray-300 px-3 py-2 placeholder-gray-500 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm dark:focus:border-indigo-500 dark:focus:ring-indigo-500`}
-      />
-      {!!children && (
-        <div className="absolute inset-y-0 right-2 flex items-center">
-          {children}
-        </div>
-      )}
+    <div>
+
+      <div className="relative">
+        <label htmlFor={name} className="sr-only">
+          {label ?? name}
+        </label>
+        <input
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          {...props}
+          ref={ref}
+          className={`relative block w-full ${!!children && "pr-8"
+            } appearance-none rounded-md border bg-gray-400 bg-opacity-10 dark:border-gray-600 border-gray-300 px-3 py-2 placeholder-gray-500 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm dark:focus:border-indigo-500 dark:focus:ring-indigo-500`}
+        />
+        {!!children && (
+          <div className="absolute inset-y-0 right-2 flex items-center">
+            {children}
+          </div>
+        )}
+      </div>
       {!!error && (
         <label className="text-red-500 text-sm">
           {error.message.toString()}
